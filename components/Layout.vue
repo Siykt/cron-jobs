2d9fb1438cfd0ba5ccb9af091111fbe513ada4fc
siykt <cnsiykt@163.com>
2023-06-14 00:01:00 +0800

    feat: 案例1

--- components/Layout.vue
+++ components/Layout.vue
@@ -7,7 +7,7 @@
     <div class="bg absolute bottom-0 left-0 right-0 top-0 h-screen w-screen bg-cover bg-center" />
     <div class="absolute bottom-0 left-0 right-0 top-0 h-screen w-screen bg-gray-800 bg-opacity-75" />
     <div
-      class="relative z-1 mx-auto h-screen w-screen flex flex-col overflow-hidden bg-#282c34 md:my-15vh md:h-auto md:w-3xl md:rounded-md"
+      class="relative z-1 mx-auto h-screen w-screen flex flex-col overflow-hidden bg-#ffffff45 md:my-15vh md:h-auto md:w-3xl md:rounded-md"
     >
       <slot />
     </div>
