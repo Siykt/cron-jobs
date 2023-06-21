2127dc470236d32b4c91cd7f9ee53cc69c745466
siykt <cnsiykt@163.com>
2023-06-21 10:17:32 +0800

    feat: 任务详情与日志

--- components/JobDetails.vue
+++ components/JobDetails.vue
@@ -1,9 +1,34 @@
 <script setup lang="ts">
-// something
+interface Props {
+  job: any
+}
+
+defineProps<Props>()
 </script>
 
 <template>
   <div class="min-h-1px w-full flex flex-col gap-2 p4 text-#ffffff60">
+    <p class="m0 flex items-center">
+      任务名称：{{ job.name }}
+    </p>
+    <p class="m0 flex items-center">
+      任务描述：
+    </p>
+    <p class="m0 flex items-center">
+      {{ job.description }}
+    </p>
+    <p class="m0 flex items-center">
+      任务配置：
+    </p>
+    <p class="m0 flex items-center">
+      [ConfigType]: String 手机号码
+    </p>
+    <p class="m0 flex items-center">
+      [ConfigType]: JSON
+    </p>
+    <p class="m0 flex items-center">
+      [Param]: phoneNumber 手机号码
+    </p>
     <p class="m0 mt4 flex items-center justify-center md:justify-start">
       ---------- 任务指令 ----------
     </p>
--- components/JobLogs.vue
+++ components/JobLogs.vue
@@ -1,10 +1,35 @@
 <script setup lang="ts">
-// something
+interface Props {
+  job: any
+}
+
+defineProps<Props>()
 </script>
 
 <template>
   <div class="min-h-1px w-full flex flex-col gap-2 p4 text-#ffffff60">
-    logs~~~
+    <div class="h-1/2 flex flex-col">
+      <p class="m0 flex items-center justify-center md:justify-start">
+        ---------- 运行日志 ----------
+      </p>
+      <ul class="not-scroll m0 mt2 max-w-full w-full flex flex-1 flex-wrap gap-2 overflow-y-scroll pl-0 pr-2">
+        <li v-for="i in 10" :key="i" class="mb-2 w-full flex flex-col gap-1 md:w-500px">
+          <span class="line-clamp-1">[Job {{ i }}] [2023.06.21 09:00:00]</span>
+          <span class="line-clamp-3">运行时打印的日志运行时打印的日志运行时打印的日志运行时打印的日志运行时打印的日志运行时打印的日志运行时打印的日志运行时打印的日志运行时打印的日志运行时打印的日志运行时打印的日志运行时打印的日志运行时打印的日志运行时打印的日志运行时打印的日志运行时打印的日志运行时打印的日志</span>
+        </li>
+      </ul>
+    </div>
+    <div class="h-1/2 flex flex-col">
+      <p class="m0 flex items-center justify-center md:justify-start">
+        ---------- 错误日志 ----------
+      </p>
+      <ul class="not-scroll m0 mt2 max-w-full w-full flex flex-1 flex-wrap gap-2 overflow-y-scroll pl-0 pr-2">
+        <li v-for="i in 10" :key="i" class="mb-2 w-full flex flex-col gap-1 md:w-500px">
+          <span class="line-clamp-1">[Job {{ i }}] [2023.06.21 09:00:00]</span>
+          <span class="line-clamp-3">运行时打印的日志运行时打印的日志运行时打印的日志运行时打印的日志运行时打印的日志运行时打印的日志运行时打印的日志运行时打印的日志运行时打印的日志运行时打印的日志运行时打印的日志运行时打印的日志运行时打印的日志运行时打印的日志运行时打印的日志运行时打印的日志运行时打印的日志</span>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
