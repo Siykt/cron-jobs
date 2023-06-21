3413c1e89e5a8fde62a9a760f64e6d67c42db7c3
siykt <cnsiykt@163.com>
2023-06-21 14:21:37 +0800

    feat: 优化样式

--- components/JobLogs.vue
+++ components/JobLogs.vue
@@ -12,8 +12,8 @@ defineProps<Props>()
       <p class="m0 flex items-center justify-center md:justify-start">
         ---------- 运行日志 ----------
       </p>
-      <ul class="not-scroll m0 mt2 max-w-full w-full flex flex-1 flex-wrap gap-2 overflow-y-scroll pl-0 pr-2">
-        <li v-for="i in 10" :key="i" class="mb-2 w-full flex flex-col gap-1 md:w-500px">
+      <ul class="not-scroll m0 mt2 max-w-full w-full flex flex-1 flex-wrap gap-2 overflow-y-scroll pl-0 pr-2 md:w-520px">
+        <li v-for="i in 10" :key="i" class="mb-2 w-full flex flex-col gap-1">
           <span class="line-clamp-1">[Job {{ i }}] [2023.06.21 09:00:00]</span>
           <span class="line-clamp-3">运行时打印的日志运行时打印的日志运行时打印的日志运行时打印的日志运行时打印的日志运行时打印的日志运行时打印的日志运行时打印的日志运行时打印的日志运行时打印的日志运行时打印的日志运行时打印的日志运行时打印的日志运行时打印的日志运行时打印的日志运行时打印的日志运行时打印的日志</span>
         </li>
@@ -23,8 +23,8 @@ defineProps<Props>()
       <p class="m0 flex items-center justify-center md:justify-start">
         ---------- 错误日志 ----------
       </p>
-      <ul class="not-scroll m0 mt2 max-w-full w-full flex flex-1 flex-wrap gap-2 overflow-y-scroll pl-0 pr-2">
-        <li v-for="i in 10" :key="i" class="mb-2 w-full flex flex-col gap-1 md:w-500px">
+      <ul class="not-scroll m0 mt2 max-w-full w-full flex flex-1 flex-wrap gap-2 overflow-y-scroll pl-0 pr-2 md:w-520px">
+        <li v-for="i in 10" :key="i" class="mb-2 w-full flex flex-col gap-1">
           <span class="line-clamp-1">[Job {{ i }}] [2023.06.21 09:00:00]</span>
           <span class="line-clamp-3">运行时打印的日志运行时打印的日志运行时打印的日志运行时打印的日志运行时打印的日志运行时打印的日志运行时打印的日志运行时打印的日志运行时打印的日志运行时打印的日志运行时打印的日志运行时打印的日志运行时打印的日志运行时打印的日志运行时打印的日志运行时打印的日志运行时打印的日志</span>
         </li>
