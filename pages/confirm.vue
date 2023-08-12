8cd6efd611697d75e27df2cb668f6e5d39bf233b
siykt <cnsiykt@163.com>
2023-08-12 11:37:47 +0800

    feat: prettier confirm page

--- pages/confirm.vue
+++ pages/confirm.vue
@@ -1,16 +1,58 @@
 <script setup lang="ts">
 const user = useSupabaseUser()
 
+const msg = ref('')
+
 watch(user, () => {
   if (user.value)
     return navigateTo('/')
 }, { immediate: true })
+
+onMounted(() => {
+  const texts = '正在登录中...'.split('')
+  let i = 0
+  const timer = setInterval(() => {
+    msg.value += texts[i++]
+    if (i === texts.length)
+      clearInterval(timer)
+  }, 150)
+})
 </script>
 
 <template>
-  <div class="flex flex-1 flex-col overflow-hidden">
-    <div class="mx-auto min-h-1px w-full flex flex-col gap-2 p4 text-#ffffff60 md:w-460px md:px8">
-      登录中...
-    </div>
+  <div class="flex flex-1 flex-col items-center overflow-hidden">
+    <h2 class="mb-2 mt-30 flex items-center text-lg text-#ffffff60">
+      <span>{{ msg }}</span>
+      <span class="blink-animate ml-1px">|</span>
+    </h2>
+    <i class="rotate-animate mdi:loading text-18 text-#6c6cc9" />
   </div>
 </template>
+
+<style lang="less" scoped>
+@keyframes rotate {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(359deg);
+  }
+}
+.rotate-animate {
+  animation: rotate 0.8s linear infinite;
+}
+@keyframes blink {
+  0% {
+    opacity: 0;
+  }
+  50% {
+    opacity: 1;
+  }
+  100% {
+    opacity: 0;
+  }
+}
+.blink-animate {
+  animation: blink 1s infinite;
+}
+</style>
